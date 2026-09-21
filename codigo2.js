d3231a6d330d5de77d92af587e1a1c3d807310a4
Ashley2810 <rabanalesanderson5@gmail.com>
2026-09-21 09:14:24 -0600

    java de inicio de sesion

--- codigo2.js
+++ codigo2.js
@@ -71,7 +71,7 @@ function iniciarSesion(){
         mensaje2.style.color = "red";
     }
     if (opcionesValidas) {
-        window.location.href = "sumas.html";
+        window.location.href = "bienvenido.html";
     }
     else {
         mensaje2.textContent = "¡Usuario o contraseña incorrectos!";
